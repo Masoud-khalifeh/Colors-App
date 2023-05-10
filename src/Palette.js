67fd49fe4c50e0250fe466f2c16b080e43a1e177
Masoud Khalifeh <masoud.khalifeh28@gmail.com>
2023-05-10 11:27:24 +0200

    add palette component

---
+++ src/Palette.js
@@ -0,0 +1,16 @@
+
+import React from "react";
+
+export default function Pallete() {
+
+    return (
+        <div className="palette">
+            {/* navbar goes here */}
+            <div className="paletteColors">{/*bunch of color boxes*/}</div>
+            {/* footer goes here             */}
+        </div>
+    )
+}
+
+
+
